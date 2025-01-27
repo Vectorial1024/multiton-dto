59a8c8b5da211232769756f5d3fb1e1d192115be
Vincent Wong <vincentwwy168@gmail.com>
2025-01-27 17:51:12 +0800

    Implement toMultiton()

--- src/MultitonDtoTrait.php
+++ src/MultitonDtoTrait.php
@@ -28,4 +28,37 @@ trait MultitonDtoTrait {
      * @return string The ID of this DTO.
      */
     abstract protected function provideDtoID(): string;
+
+    /**
+     * The multiton table for storing the DTO instances.
+     * @var array<string, WeakReference<self>>
+     */
+    private static $multitonTable = [];
+
+    /**
+     * Returns the shared multiton DTO instance with the same ID of `$this`. This therefore deduplicates `$this`.
+     * 
+     * If no such DTO instance exists, then `$this` is remembered for future sharing.
+     * @return self The "flattened" multiton DTO instance.
+     */
+    final public function toMultiton(): self
+    {
+        $dtoID = $this->getDtoID();
+        // checks whether the existing WeakReference is valid
+        $existingRef = static::$multitonTable[$dtoID] ?? null;
+        if ($existingRef == null) {
+            // first time appearing; remember it and return
+            static::$multitonTable[$dtoID] = WeakReference::create($this);
+            return $this;
+        }
+        // we have a previous reference; see if it is still valid
+        $theInstance = $existingRef->get();
+        if ($theInstance === null) {
+            // previous instance expired; remember it and return
+            static::$multitonTable[$dtoID] = WeakReference::create($this);
+            return $this;
+        }
+        // previous instance still valid; return that
+        return $theInstance;
+    }
 }
